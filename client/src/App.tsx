4f3d4228b8f6f684a4014367b64be7dbb1d08cf8
noliver-ykc <noliver.ykc@gmail.com>
2023-06-19 01:28:39 +0000

    initialize app

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+const apiKey ="egp5drfwjm2x"
+
+function App() {
+  return (
+    <div className="app__wrapper">
+      <p>Hello World</p>
+    </div>
+  );
+}
+
+export default App;
